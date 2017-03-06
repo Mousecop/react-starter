53a9bd9ba3832a454cb4a201e8cf3133a928cd4a
Mousecop <joaskew13@gmail.com>
2017-03-06 12:06:10 -0500

    Card component

---
+++ js/components/card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Card () {
+    const text = "this is a card!";
+    return (
+        <div className="card">
+            {text}
+        </div>
+    );
+}
