1926e56e7e77aac1ceb7ca84f762ce452352146e
Mousecop <joaskew13@gmail.com>
2017-03-07 12:46:05 -0500

    make into class, pass new props

--- js/components/List.js
+++ js/components/List.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import Card from './Card'
 
-export default function List(props) {
-    const cardList = []
-    for(let i=0; i<3; i++) {
-        cardList.push(<Card text={`This is card ${i+1}`}/>);
+export default class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onChange = this.onChange.bind(this);
+        this.onMySubmit =  this.onMySubmit.bind(this);
     }
-    return (
-        <div className="list">
-        <h3 className="listTitle">{props.listTitle}</h3>
-            {cardList}
-        </div>
-    )
+    
+    onChange() {
+        this.props.onChange();
+    }
+    onMySubmit() {
+        this.props.onMySubmit();
+    }
+    render(){
+        const cardList = [];
+        for(let i=0; i<3; i++) {
+            cardList.push(<Card text={`This is card ${i+1}`}/>);
+        };
+        // const props = this.props; //redefine props in render function to use props insides.
+        return (
+            <div className="list">
+                <h3 className="listTitle">{this.props.listTitle}</h3>
+                {cardList}
+                <form className="elFormo">
+                    <input type="text" onChange={this.onChange} />
+                    <button type="submit" onClick={e => e.preventDefault()} onMySubmit={this.onMySubmit}>Click Me!</button>
+                </form>
+            </div>
+        );
+    }
 }
