1209fd2bba2a870bae0a1dea79daa8fb803202da
Mousecop <joaskew13@gmail.com>
2017-03-07 12:45:27 -0500

    make into stateful component

--- js/components/Board.js
+++ js/components/Board.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import List from './List'
 
-export default function Board(props) {
-    const boardList = [];
-    for(let i=0; i<3; i++) {
-        boardList.push(<List listTitle={`Title of list ${i+1}`}/>);
+export default class Board extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    
+    onAddInputChanged() {
+        console.log("Input Changed");
+    }
+    onAddSubmit() {
+        console.log("I was clicked!");
     }
 
+    render() {
+        const boardList = [];
+        for(let i=0; i<3; i++) {
+            boardList.push(<List listTitle={`Title of list ${i+1}`} onChange={this.onAddInputChanged} onMySubmit={this.onAddSubmit}/>);
+        }
+    const props = this.props;
     return (
         <div className="board">
         <h3 className="boardTitle">{props.boardTitle}</h3>
             {boardList}
         </div>
-    )
+    );
+    }
+    
 }
